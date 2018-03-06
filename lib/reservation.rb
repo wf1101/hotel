83d17e16123931a7a5a872a35eea36d4591586f7
Wenjie <traxiaojie@gmail.com>
2018-03-05 20:36:51 -0800

    Add class Reservation Add constructor, overlap? and get_cost method

---
+++ lib/reservation.rb
@@ -0,0 +1,22 @@
+class Reservation
+  attr_reader :start_date, :end_date, :room
+  def initialize(start_date, end_date, room)
+
+    @start_date = start_date
+    @end_date = end_date
+    @room = room
+    # @guest = guest
+  end
+
+  def overlap?(first_date, second_date)
+    flag = true
+    return false if first_date >= @end_date || second_date <= @start_date
+    return flag
+  end
+
+  def get_cost
+    return (end_date - start_date) * 200
+  end
+
+
+end
