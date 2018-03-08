9f8f6bae798acc7d034895f49ca987fe1dab2f12
Wenjie <traxiaojie@gmail.com>
2018-03-07 21:36:46 -0800

    Removed all instance methods

--- lib/room.rb
+++ lib/room.rb
@@ -1,30 +1,30 @@
 require 'pry'
 class Room
-  attr_reader :id, :reservations
+  attr_reader :id
   attr_accessor :rate
   def initialize(id)
     @id = id
-    @reservations = []
+    # @reservations = []
     @rate = 200
   end
 
-  def available?(start_date, end_date)
-    @reservations.each do |reservation|
-      if reservation.overlap?(start_date, end_date)
-        return false
-      end
-    end
-
-    return true
-  end
-
-  def find_reservation(date)
-    reservations_on_date = @reservations.select {|reservation| (reservation.start_date...reservation.end_date).include?(date)}
-    return reservations_on_date
-  end
-
-  def add_reservation(reservation)
-    @reservations << reservation
-  end
+  # def available?(start_date, end_date)
+  #   @reservations.each do |reservation|
+  #     if reservation.overlap?(start_date, end_date)
+  #       return false
+  #     end
+  #   end
+  #
+  #   return true
+  # end
+  #
+  # def find_reservation(date)
+  #   reservations_on_date = @reservations.select {|reservation| (reservation.start_date...reservation.end_date).include?(date)}
+  #   return reservations_on_date
+  # end
+  #
+  # def add_reservation(reservation)
+  #   @reservations << reservation
+  # end
 
 end
