d649d56cc167525a1f9256080ec17f1ae25ed58d
Wenjie <traxiaojie@gmail.com>
2018-03-07 21:36:07 -0800

    Removed methods to find available rooms and reserve a room in the blcok

--- lib/block.rb
+++ lib/block.rb
@@ -9,25 +9,25 @@ class Block
     @rooms = rooms
   end
 
-  def find_available_rooms(start_date, end_date)
-    unbooked_rooms = []
-    @rooms.each do |room|
-      if room.available?(start_date, end_date)
-        unbooked_rooms << room
-      end
-    end
-    return unbooked_rooms
-  end
+  # def find_available_rooms(start_date, end_date)
+  #   unbooked_rooms = []
+  #   @rooms.each do |room|
+  #     if room.available?(start_date, end_date)
+  #       unbooked_rooms << room
+  #     end
+  #   end
+  #   return unbooked_rooms
+  # end
 
-  def find_next_room
-    room = available_rooms.first
-  end
+  # def find_next_room
+  #   room = available_rooms.first
+  # end
 
-  def reserve
-    new_reservation = Reservation.new(@start_date, @end_date, next_room)
-
-    next_room.reservations << new_reservation
-    return new_reservation
-  end
+  # def reserve(room)
+  #   new_reservation = Reservation.new(@start_date, @end_date, next_room)
+  #
+  #   # next_room.reservations << new_reservation
+  #   return new_reservation
+  # end
 
 end
