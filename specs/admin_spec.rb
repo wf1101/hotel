d4c1c4b310220c36a7c4f84b319ae3b091fd6d76
Wenjie <traxiaojie@gmail.com>
2018-03-07 22:47:25 -0800

    Implemented tests for creating block and reserving room in block

--- specs/admin_spec.rb
+++ specs/admin_spec.rb
@@ -1,16 +1,18 @@
 require_relative 'spec_helper'
+require 'pry'
 
 describe "Admin class" do
   describe "initialize" do
-    it "can create an instance of Hotel with 20 rooms" do
+    it "can create an instance of Admin with 20 rooms and list of each room's revervations" do
       s_hotel = Admin.new
       s_hotel.must_be_instance_of Admin
 
-      s_hotel.rooms.each do |room|
+      s_hotel.rooms_reservations.each do |room, reservations|
         room.must_be_instance_of Room
+        reservations.must_be_instance_of Array
       end
 
-      s_hotel.rooms.length.must_equal 20
+      s_hotel.rooms_reservations.keys.length.must_equal 20
     end
   end
 
@@ -24,16 +26,16 @@ describe "Admin class" do
       new_reservation.must_be_instance_of Reservation
     end
 
-    it "can add the new reservation to the room's reservations" do
+    it "can add the new reservation to the rooms_reservations hash" do
       s_hotel = Admin.new
       date_in = Date.new(2018,5,20)
       date_out = Date.new(2018,5,25)
       new_reservation = s_hotel.create_reservation(date_in, date_out)
 
-      new_reservation.room.reservations.must_include new_reservation
+      s_hotel.rooms_reservations[new_reservation.room].must_include new_reservation
     end
 
-    it "can reserve a room stars at the same day that another reservation ends" do
+    it "can reserve a room starts at the same day that another reservation ends" do
       s_hotel = Admin.new
       date_in = Date.new(2018,6,20)
       date_out = Date.new(2018,6,25)
@@ -65,15 +67,27 @@ describe "Admin class" do
       }.must_raise StandardError
     end
 
-    it "can change the room's status to unavailable" do
+    it "will not reserve a room that is in a block" do
       s_hotel = Admin.new
-      date_in = Date.new(2018,5,20)
-      date_out = Date.new(2018,5,25)
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number = 4
+      rate = 188
+      w_block = s_hotel.create_block(date_in, date_out, number, rate)
+      # binding.pry
       new_reservation = s_hotel.create_reservation(date_in, date_out)
-
-      result = new_reservation.room.available?(date_in, date_out)
-      result.must_equal false
+      w_block.rooms.wont_include new_reservation.room
     end
+
+    # it "can change the room's status to unavailable" do
+    #   s_hotel = Admin.new
+    #   date_in = Date.new(2018,5,20)
+    #   date_out = Date.new(2018,5,25)
+    #   new_reservation = s_hotel.create_reservation(date_in, date_out)
+    #
+    #   result = new_reservation.room.available?(date_in, date_out)
+    #   result.must_equal false
+    # end
 
     it "can raise an error when no room is available" do
       s_hotel = Admin.new
@@ -97,7 +111,6 @@ describe "Admin class" do
       s_hotel.create_reservation(date_in, date_out)
     end
     next_room = s_hotel.find_next_room(date_in, date_out)
-
 
     it "returns an instance of room" do
       next_room.must_be_instance_of Room
@@ -199,4 +212,136 @@ describe "Admin class" do
 
     end
   end
+
+  describe "create_block method" do
+    it "can create a block of rooms when rooms are available" do
+      s_hotel = Admin.new
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number = 4
+      rate = 188
+      w_block = s_hotel.create_block(date_in, date_out, number, rate)
+
+      w_block.must_be_instance_of Block
+      w_block.rooms.length.must_equal 4
+    end
+
+    it "can raise an error when rooms are not available" do
+      s_hotel = Admin.new
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number = 4
+      rate = 188
+
+      17.times do
+        s_hotel.create_reservation(date_in, date_out)
+      end
+
+      proc{
+        s_hotel.create_block(date_in, date_out, number, rate)
+      }.must_raise ArgumentError
+    end
+
+    it "can raise an error when creating a block of more than 5 rooms or less than 3 rooms" do
+      s_hotel = Admin.new
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number_1 = 2
+      number_2 = 7
+      rate = 188
+
+      proc{
+        s_hotel.create_block(date_in, date_out, number_1, rate)
+      }.must_raise ArgumentError
+      proc{
+        s_hotel.create_block(date_in, date_out, number_2, rate)
+      }.must_raise ArgumentError
+    end
+  end
+
+  describe "find_block_rooms method" do
+    it "accurately find the given number of rooms to create a block" do
+      s_hotel = Admin.new
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number = 4
+
+      rooms = s_hotel.find_block_rooms(date_in, date_out, number)
+      rooms.length.must_equal 4
+      rooms.each do |room|
+        room.must_be_instance_of Room
+      end
+    end
+
+    it "won't include a room in other block" do
+      s_hotel = Admin.new
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number = 4
+      number_2 = 3
+      rate = 188
+      w_block = s_hotel.create_block(date_in, date_out, number, rate)
+
+      rooms = s_hotel.find_block_rooms(date_in, date_out, number_2)
+      w_block.rooms.each do |room|
+        rooms.wont_include room
+      end
+    end
+  end
+
+  describe "list_blocked_rooms method" do
+    it "can return a list of blocked_rooms for certain date range" do
+      s_hotel = Admin.new
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number = 4
+      number_2 = 3
+      rate = 188
+
+      w_block = s_hotel.create_block(date_in, date_out, number, rate)
+      v_block = s_hotel.create_block(date_in, date_out, number_2, rate)
+
+      blocked_rooms = s_hotel.list_blocked_rooms(date_in, date_out)
+      blocked_rooms.must_be_instance_of Array
+      blocked_rooms.length.must_equal number + number_2
+      (w_block.rooms + v_block.rooms).each do |w_v_room|
+        blocked_rooms.must_include w_v_room
+      end
+    end
+  end
+
+  describe "check_block_rooms method" do
+    it "return a list of available rooms in a block" do
+      s_hotel = Admin.new
+      date_in = Date.new(2018,6,20)
+      date_out = Date.new(2018,6,25)
+      number = 4
+      rate = 188
+
+      w_block = s_hotel.create_block(date_in, date_out, number, rate)
+      # binding.pry
+      s_hotel.reserve_block_room(w_block)
+      s_hotel.reserve_block_room(w_block)
+
+      rooms = s_hotel.check_block_rooms(w_block)
+      # binding.pry
+      rooms.length.must_equal 2
+      w_block.rooms.length.must_equal 4
+      # rooms[0].must_equal w_block.rooms[2]
+      # rooms[1].must_equal w_block.rooms[3]
+    end
+  end
+
+  describe "find_next_block_room method" do
+    it "return firt available room in a block" do
+
+    end
+  end
+
+  describe "reserve_block_room method" do
+    it "can reserve a room in a block" do
+
+    end
+  end
+
 end
